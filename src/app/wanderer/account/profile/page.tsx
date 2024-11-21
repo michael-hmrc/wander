feb90d714fbe14d4d361a41c6a2f528a407d9a5a
mywyau <47248104+mywyau@users.noreply.github.com>
2024-11-21 14:39:50 +0000

    wip checkpoint

--- src/app/wanderer/account/profile/page.tsx
+++ src/app/wanderer/account/profile/page.tsx
@@ -240,20 +240,6 @@ export default function ProfilePage() {
                 />
               </div>
 
-
-              {/* <div>
-                <label htmlFor="address.city" className="block text-sm font-medium text-gray-700 mb-1">City</label>
-                <input
-                  type="text"
-                  id="address.city"
-                  value={userData.userAddress.city || ""}
-                  onChange={handleChange}
-                  className="w-full px-4 py-2 border border-gray-300 rounded-md"
-                  disabled={!isEditing}
-                />
-              </div> */}
-
-
               <div>
                 <label htmlFor="address.city" className="block text-sm font-medium text-gray-700 mb-1">City</label>
                 <select
@@ -383,7 +369,6 @@ export default function ProfilePage() {
 
               {/* Add more fields for address */}
             </ProfileSection>
-
 
             <div className="col-span-full">
               <button
